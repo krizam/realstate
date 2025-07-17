1ff8fabc6ff842ec2f5e761c03f660d2ec6005ae
Krizam Byanju <159673964+krizam@users.noreply.github.com>
2025-07-17 14:45:30 +0545

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'https://realstate-80g2.onrender.com',
+        target: 'http://localhost:3000',
         secure: false,
        
        
